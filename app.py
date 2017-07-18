7b553ed3f4372743f17b3b76747fe02784a5d5bc
parksangkyo <rapkyo01@naver.com>
2017-07-18 14:02:52 +0900

    initial commit

--- app.py
+++ app.py
@@ -1,18 +1,55 @@
 import os, sys
 from flask import Flask, request
+from pymessenger import Bot
 
 app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "EAAZAfvkVBy8sBADgMzL6jSYK77kEOPRupeQOzsCS12qJN5CmyvcyLsBLseIiA5xqQKAuuDABEWEzEcgld0mGh0NGgCCfPniZCy9OgN87YL13npeAGAqIZC6sPuQxLMiEh9Q2ZB4yxSdcVmpyGT2PeWDltaC7SK0x7Qxd4ifxTiO2xZAp0m8UQ"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 @app.route('/', methods=['GET'])
 def verify():
     # Webhook verification
-    if request.args.get("hud.mode") == "subscribe" and request.args.get("hub.challenge"):
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
     return "Hello world", 200
 
-
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    log(data)
+    
+    if data ['object'] == 'page' :
+        for entry in data ['entry']:
+            for messaging_event in entry['messaging']:
+                
+                # Ids
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+                
+                if messaging_event.get('message'):
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+                        
+                        
+                    #Echo
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+                
+    
+    
+    return "ok", 200
+    
+def log(message):
+    print(message)
+    sys.stdout.flush()
+    
+    
 if __name__ == "__main__":
     app.run(debug = True, port = 80)
     
